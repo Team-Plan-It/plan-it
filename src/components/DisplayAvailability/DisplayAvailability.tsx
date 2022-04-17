0f52743641d2889e83daa563d6c5e5ab4e4a4b77
kristinmc42 <mccollum.k.j@gmail.com>
2022-04-17 13:52:38 -0400

    added try catch to handle error in DisplayAvailabilities component

--- src/components/DisplayAvailability/DisplayAvailability.tsx
+++ src/components/DisplayAvailability/DisplayAvailability.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 // import axios from "axios";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { DayPilot, DayPilotCalendar, DayPilotCalendarComponent } from "@daypilot/daypilot-lite-react";
+import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
 
 
 //components
@@ -19,39 +19,61 @@ import availabilities from "../../testEvents";
 
 
 const DisplayAvailability = () => {
-  let calendar:any|void = DayPilot.Calendar;
+  let calendar = DayPilot.Calendar;
   
-  const colorArray:string[] = ["#ff5733", "#ffb533", "#fcff33", "#33ff46", "#33f6ff", "#3361ff", "#d733ff"];
   
   //initialize state
   const [eventArray, setEventArray] = useState(availabilities);
+
+
   
   useEffect(() => {
-     if(eventArray){
+    const colorArray:string[] = ["#ff5733", "#ffb533", "#fcff33", "#33ff46", "#33f6ff", "#3361ff", "#d733ff"];
+
+    let eventList = [...eventArray];
+    console.log(eventList);
+     try{
        // map through each object in the events array
-       eventArray.forEach((obj, index) => {
+       eventList.forEach((obj, index) => {
          // assign a color for each user object
          let color:string = colorArray[index];
-         
-         obj.dates.forEach((date:string[]) => {
-           // in each object, map through the array of dates to create new Date Pilot events
-           let newEvent:any[] = new DayPilot.Event({
-             start: date[0],
-             end: date[1],
-             id: DayPilot.guid(),
-             text: obj.user,
-             backColor: color,
-             fontColor: "#000000",
-            });
-            // add the new event to the events list
-            calendar.events.add(newEvent);
-            console.log(newEvent)
-         })
+         if (obj === undefined){
+           console.log("obj is undefined", index)
+         }else {
+           console.log(obj, index)
+            obj.dates.forEach((date:string[]) => {
+              // in each object, map through the array of dates to create new Date Pilot events
+              if(date === undefined){
+                console.log("date is undefined", date)
+
+              }else {
+                console.log(date)
+                let newEvent:any[] = new DayPilot.Event({
+                  start: date[0],
+                  end: date[1],
+                  id: DayPilot.guid(),
+                  text: obj.user,
+                  backColor: color,
+                  fontColor: "#000000",
+                 });
+                //  add the new event to the events list
+                 calendar.events.add(newEvent);
+                 console.log(newEvent)
+                 if(calendar === undefined){
+                   console.log("calendar is undefined");
+                 }else {
+                   console.log(calendar)
+                 }
+              }
+            })
+         }
         })
+     }catch{
+       console.log("error")
      }
 
  
-  }, [])
+  }, [eventArray, calendar])
 
   return(
     <div className="display">
@@ -67,7 +89,7 @@ const DisplayAvailability = () => {
         <div className="displayCalendar">
           <DayPilotCalendar 
             durationBarVisible={false}
-            // startDate={"2022-04-11"}
+            startDate={"2022-05-09T09:00:00"}
             viewType={"Week"}
             headerDateFormat={"d MMMM yyyy"}
             eventArrangement={"Full"}
