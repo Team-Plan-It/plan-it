f7e581b42d3afdb9ff16a554406dea2e8db79920
kristinmc42 <mccollum.k.j@gmail.com>
2022-05-03 11:28:42 -0400

    fixed calendar display issue

--- src/components/DisplayAvailability/DisplayAvailResults.tsx
+++ src/components/DisplayAvailability/DisplayAvailResults.tsx
@@ -9,9 +9,11 @@ import { useLocation } from "react-router-dom";
 //components
 import meetingData from "../../testEventsMeetingInfo";
 import Sidebar from "../Sidebar/Sidebar";
+import Overlap from "./overlap";
 
 //styles
 import "./DisplayAvailability.css";
+
 
 
 
@@ -40,13 +42,13 @@ type EventObj = {
   end: number;
 }
 interface AvailabilityArray{
-  sunday: UserInfo[];
-  monday: UserInfo[];
-  tuesday: UserInfo[];
-  wednesday: UserInfo[];
-  thursday: UserInfo[];
-  friday: UserInfo[];
-  saturday: UserInfo[];
+  sunday?: UserInfo[];
+  monday?: UserInfo[];
+  tuesday?: UserInfo[];
+  wednesday?: UserInfo[];
+  thursday?: UserInfo[];
+  friday?: UserInfo[];
+  saturday?: UserInfo[];
 }
 
 interface MeetingInfo {
@@ -92,6 +94,10 @@ const DisplayAvailResults = () => {
   const [ arrayOfEvents, setArrayOfEvents ] = useState<any[]>();
   // calendar month
   const [ month, setMonth ] = useState<string>();
+  // loading
+  const [ isLoading, setIsLoading ] = useState<boolean>(true);
+  // availability by date
+  const [ availabilityByDay, setAvailabilityByDay ] = useState<AvailabilityArray>();
 
 
   // arrays of each users events
@@ -103,80 +109,94 @@ const DisplayAvailResults = () => {
   const [ user6eventArray, setUser6eventArray ] = useState<UserInfo>();
 
 
-
-  // axios call in async function
-  const getData = async () => {
-    const response = await axios.get(`http://localhost:4000/dates/results/${meetingNumID}`);
-   
-      const data = response.data[0];
-      console.log(data.eventName)
-      // setDataObj(data)
-      const { eventName, length, date, timezone, emails, meetingNumber, users} = data!;
-
-      // set event created to false
-      setEventCreated(false)
-      // save data in state
-      setEventName(eventName);
-      setMeetingLength(length);
-      setSelectedDate(date);
-      setCoordTimeZone(timezone);
-      setMeetingNumber(meetingNumber);
-      setUserInfoData(users);
-
-      // populate userNameArray 
-      const userNamesArray:(string | undefined)[] = users.map((user:UserInfo) => {
-          return user.userName;
-      });
-      setUserNames(userNamesArray);
-
-      // determine number of meeting attendees
-      // includes coordinator
-      let arrayOfNumOfUsers = [1];
-      if(emails.length > 0){
-        for (let i= 0; i < emails.length; i++){
-          arrayOfNumOfUsers.push(i + 2)
-        }
-        setNumOfAttendees(arrayOfNumOfUsers);
-      }
-
-      // get month as string from event date
-      const month = new Date(date).toLocaleString('default', {month: "long"});
-      setMonth(month);
-
-     console.log("about to call createEventList")
-    //  setTimeout(() => {
-       createEventList(users);
-      //  if(userInfoData){
-      //    createEventList();
-   
-      //  }else{
-      //    console.log("userInfoData was not true")
-      //  }
-       
-    //  }, 2000);
-
-    // console.log("about to call createEventList")
-  
- 
-    }
-
-  useEffect(() => {
+    useEffect(() => {
+      console.log("inside useEffect")
      getData();
     
   }, [])
 
-  // useEffect(() => {
+  // axios call in async function
+  const getData = async () => {
+    try{
+      console.log("isLoading: ", isLoading)
+        const response = await axios.get(`http://localhost:4000/dates/results/${meetingNumID}`);
+        
+          console.log("in try of getData function with axios call")
+          console.log(response)
+         if(response !== undefined){
+           setIsLoading(false);
+           // const data = response.data[0];
+            console.log("isLoading: ", isLoading)
+           // console.log(response.data[0].eventName)
+           
+           
+           // deconstruct info from data
+           const { eventName, length, date, timezone, emails, meetingNumber, users, availabilityArray } = response.data[0]!;
+           
+           // set event created to false
+           setEventCreated(false)
+           // save data in state
+           setEventName(eventName); 
+           setMeetingLength(length);
+           setSelectedDate(date);
+           setCoordTimeZone(timezone);
+           setMeetingNumber(meetingNumber);
+           setUserInfoData(users);
+           setAvailabilityByDay(availabilityArray);
+           
+           // populate userNameArray 
+           const userNamesArray:(string | undefined)[] = users.map((user:UserInfo) => {
+             return user.userName;
+            });
+            setUserNames(userNamesArray);
+            
+            // determine number of meeting attendees
+            // includes coordinator
+            let arrayOfNumOfUsers = [1];
+            if(emails.length > 0){
+              for (let i= 0; i < emails.length; i++){
+                arrayOfNumOfUsers.push(i + 2)
+              }
+              setNumOfAttendees(arrayOfNumOfUsers);
+            }
+            
+            // get month as string from event date
+            const month = new Date(date).toLocaleString('default', {month: "long"});
+            setMonth(month);
+            
+            // console.log("about to call createEventList")
+            // createEventList(users);
+          }else{
+            console.log("response undefined")
+          }
+        
 
-  //    console.log("about to call createEventList")
+    }catch(error:unknown){
+      if(error instanceof Error){
+        console.log("error message: ", error.message)
+      }
+    }
+  }
+
+
+
+  useEffect(() => {
+
+     console.log("about to call createEventList")
   
-  //      if(userInfoData){
-  //        createEventList();
+       if(userInfoData && !eventCreated){
+         createEventList(userInfoData);
+     
+        calendar.update();
+        console.log(calendar.events.list)
+       }else{
+         console.log("userInfoData undefined")
+         console.log("eventCreate: ", eventCreated)
+       }
+    
+  }, [userInfoData])
 
-  //      }
-       
- 
-  // }, [userInfoData])
-
+  
 
   
 
@@ -202,9 +222,10 @@ const DisplayAvailResults = () => {
 
                   setUser1eventArray(user)
   
+                  
                    // loop through the availability for the user
-                  user1array.availability!.forEach(availBlock => {
-
+                  user1array.availability!.forEach((availBlock, index) => {
+                    console.log("index:", index)
                     console.log(user1array.userName!.charAt(0))
                     //  create a new event for each availability block
                     let newEvent:any[] = new DayPilot.Event({
@@ -218,9 +239,15 @@ const DisplayAvailResults = () => {
                       className:"target",
                       ref:"user1"
                      });
+                     console.log("new event was created", newEvent)
                     //  add the new event to the events list
-                     calendar.events.add(newEvent);
-  
+                    if (calendar){
+                      calendar.events.add(newEvent);
+                      console.log("calendar should be initialized")
+                    }else{
+                      console.log("calendar not initialized")
+                    }
+   
                      eventArray.push(newEvent);
                   })
                   break;
@@ -357,6 +384,7 @@ const DisplayAvailResults = () => {
              }
   
               if (index === userData!.length -1){
+                console.log("index is the length of the userData array")
                 setEventCreated(true)
                 setArrayOfEvents(eventArray)
                 calendar.events.update();
@@ -413,27 +441,40 @@ const DisplayAvailResults = () => {
           </>
           </div>
           <div className="calendar" id="calendar">
-
+            {
+              isLoading && !userInfoData && eventCreated
+              ? <p>Is loading.....</p>
+    
+              : <DayPilotCalendar 
+                      durationBarVisible={false}
+                      startDate={selectedDate}
+                      // viewType={"WorkWeek"}
+                      viewType = {"Week"}
+                      headerDateFormat={"ddd dd"}
+                      heightSpec={"Full"}
+                      showToolTip={"true"}
+                      id={"calendar"}
+                      // onEventClick={handleEventClick}
+                      cellHeight={15}
+                      // autoRefreshEnabled = {true}
+                      timeRangeSelectedHandling={"Disabled"}
+                      // onBeforeEventRender={(args: any) => {
+                      //   console.log(args)
+                      //   if(args.data.id === "user1"){
+                      //     console.log("user1")
+                      //     args.data.backColor = "#ff3db1"
+                      //   }
+                      // }}
+                      ref={(component:any | void) => {
+                        calendar = component && component.control;
+                      }} 
+                  />
+            }
           </div>
-             <DayPilotCalendar 
-            durationBarVisible={false}
-            startDate={"2022-05-09T09:00:00"}
-            // viewType={"WorkWeek"}
-            viewType = {"Week"}
-            headerDateFormat={"ddd dd"}
-            heightSpec={"Full"}
-            showToolTip={"true"}
-            id={"calendar"}
-            // onEventClick={handleEventClick}
-            cellHeight={15}
-            autoRefreshEnabled = {true}
-            // beforeCellRender={(args: any) => {createEventList()}}
-            ref={(component:any | void) => {
-              calendar = component && component.control;
-            }}
-          />
        
-         
+         <div className="overlap">
+           {/* <Overlap dayAvailabilityArray={availabilityByDay!}/> */}
+         </div>
         </div>
       </div>
     </div>
--- src/components/DisplayAvailability/overlap.tsx
+++ src/components/DisplayAvailability/overlap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useState, useEffect} from "react";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { DayPilot, DayPilotCalendar } from "@daypilot/daypilot-lite-react";
@@ -11,10 +11,28 @@ type Availability = {
     text: string;
   }
 type UserInfo = {
-  userName: string;
-  timeZone: string;
-  availability: Availability[];
+  userName?: string;
+  timeZone?: string;
+  availability?: Availability[];
+  id?: string;
 } 
+
+
+
+interface AvailabilityArray {
+  sunday?: UserInfo[];
+  monday?: UserInfo[];
+  tuesday?: UserInfo[];
+  wednesday?: UserInfo[];
+  thursday?: UserInfo[];
+  friday?: UserInfo[];
+  saturday?: UserInfo[];
+}
+
+interface AllAvail{
+  dayAvailabilityArray: AvailabilityArray
+}
+
 
 type DayEvent = {
   user: string;
@@ -27,114 +45,200 @@ type EventObj = {
   end: number;
 }
 
-const overlap = (props:any) => {
+type TimeArray = {
+  time: string;
+  array:EventObj[];
+}
+
+const Overlap:React.FC<AllAvail> = ({ dayAvailabilityArray }) => {
+  // state
+  const [ availabilityByDay , setAvailabilityByDay ] = useState<AvailabilityArray>()
+  const [ allAvailabilityTimeArrays , setAllAvailabilityTimeArrays ] = useState<TimeArray[]>();
+
+  useEffect(() => {
+    setAvailabilityByDay(dayAvailabilityArray);
+  }, [])
 
 
   // create an array for each day with 48 slots 
   // each slot should have a time value
-  const createTimeArray = () => {
-    let timeSlotArray:{time: string, array:EventObj[]}[] = [];
-    let timeDisplay = "";
+  // const createTimeArray = () => {
+  //   let timeSlotArray:TimeArray[] = [];
+  //   let timeDisplay = "";
      
-    for (let i = 0; i < 48; i++) {
+  //   for (let i = 0; i < 48; i++) {
+  //     let counter = 0;
+  //     if (i === 0) {
+  //       timeDisplay = "00:00:00";
+  //     } else {
+  //       if (i % 2 !== 0) {
+  //         counter++;
+  //       }
+  //       timeDisplay =
+  //         i % 2 === 0 ? `${(i - counter) / 2}:00:00` : `${(i - counter) / 2}:30:00`;
+  //     }
+  //     timeSlotArray.push({ time: timeDisplay,  array: [] });
+  //   }
+  //   return timeSlotArray;
+  // }
+
+  const createTimeArrayAmPm = () => {
+    let timeArrayAmPm:{time: string, array:EventObj[]}[] = [];
+    let amPmTime:string = "";
+
+    for(let i = 0; 1 < 48; i++){
       let counter = 0;
-      if (i === 0) {
-        timeDisplay = "00:00:00";
-      } else {
-        if (i % 2 !== 0) {
+      if ( i === 0){
+        amPmTime = "12:00 am - 12:30 am"
+      }else {
+         if (i % 2 !== 0) {
           counter++;
         }
-        timeDisplay =
-          i % 2 === 0 ? `${(i - counter) / 2}:00:00` : `${(i - counter) / 2}:30:00`;
-      }
-      timeSlotArray.push({ time: timeDisplay,  array: [] });
-    }
-    return timeSlotArray;
-  }
-
-
-  // calculate overlapping times when all users available
-  useEffect(() => {
-    // go through each user and sort into each day
-    let day0array:DayEvent[]= [];
-    let day1array:DayEvent[] = [];
-    let day2array:DayEvent[] = [];
-    let day3array:DayEvent[] = [];
-    let day4array:DayEvent[] = [];
-    let day5array:DayEvent[] = [];
-    let day6array:DayEvent[] = [];
-    let day7array:DayEvent[] = [];
-
-
-    try{
-      props.userInfoData!.forEach((user:any) => {
-        console.log(user)
-       // go through each user's availability
-       user.availability.forEach((timeBlock:Availability) => {
-        // push the user name and timeBlock to the array for that day of the week according to id
-           switch(timeBlock.id){
-             case 0:
-               day0array.push({user: user.userName, availability: timeBlock});
-               break
-             case 1:
-               day1array.push({user: user.userName, availability: timeBlock});  
-               break         
-             case 2:
-               day2array.push({user: user.userName, availability: timeBlock});
-               break
-             case 3:
-               day3array.push({user: user.userName, availability: timeBlock});
-               break
-             case 4:
-               day4array.push({user: user.userName, availability: timeBlock});   
-               break
-             case 5:
-               day5array.push({user: user.userName, availability: timeBlock});  
-               break
-             case 6:
-               day6array.push({user: user.userName, availability: timeBlock});  
-               break
-             case 7:
-               day7array.push({user: user.userName, availability: timeBlock});    
-               break 
-           }
-          })
-          console.log(day1array, day2array, day0array, day3array, day4array, day5array, day6array, day7array)
-     })
-
-     // **  MAKE A FUNCTION FOR THIS AND SEND EACH DAY ARRAY TO IT 
-     // for each day, loop through array add user to the timeslot array for that day 
-     // create array for day
-     const day1results = createTimeArray();
-     // loop through array for that day
-     day1array.forEach(event => {
-       // get start and end for each event as Date object
-       let startObj = new DayPilot.Date(event.availability.start);
-       let endObj = new DayPilot.Date(event.availability.end);
-       let start = 
-          startObj.getHours() * 2 + 
-          (startObj.getMinutes() === 0 ?0 :1);
-        let end =
-          endObj.getHours() * 2 +
-          (endObj.getMinutes() === 0 ? 0 : 1);
-        // add user to day1results for the timeblock start and end
-        for(let i=start; i < end; i++){
-          day1results[i].array.push({user: event.user, start: start, end: end})
+        if(i < 24){
+          amPmTime =
+          i % 2 === 0 ? `${(i - counter) / 2}:00 am` : `${(i - counter) / 2}:30 am`;
+        }else{
+          amPmTime =
+          i % 2 === 0 ? `${(i - counter) / 2}:00 pm` : `${(i - counter) / 2}:30 pm`;
         }
-        //  console.log(day1results)
+      }
+       timeArrayAmPm.push({ time: amPmTime,  array: [] });
+      }
+      return timeArrayAmPm;
+    }
+  
+  // calculate overlapping times when all users available
+ 
+    // create an array for each day with timeslots 
+    let day0array:TimeArray[] = createTimeArrayAmPm();
+    let day1array:TimeArray[] = createTimeArrayAmPm();
+    let day2array:TimeArray[] = createTimeArrayAmPm();
+    let day3array:TimeArray[] = createTimeArrayAmPm();
+    let day4array:TimeArray[] = createTimeArrayAmPm();
+    let day5array:TimeArray[] = createTimeArrayAmPm();
+    let day6array:TimeArray[] = createTimeArrayAmPm();
+
+    let weekArray:TimeArray[][] = [];
+
+
+
+
+
+        // loop through day array
+       // go through each user's availability
+      //  day.forEach()
+      //  day.availability.forEach((timeBlock:Availability) => {
+      //   // push the user name and timeBlock to the array for that day of the week according to id
+      //      switch(timeBlock.id){
+      //        case 0:
+      //          day0array.push({user: user.userName, availability: timeBlock});
+      //          break
+      //        case 1:
+      //          day1array.push({user: user.userName, availability: timeBlock});  
+      //          break         
+      //        case 2:
+      //          day2array.push({user: user.userName, availability: timeBlock});
+      //          break
+      //        case 3:
+      //          day3array.push({user: user.userName, availability: timeBlock});
+      //          break
+      //        case 4:
+      //          day4array.push({user: user.userName, availability: timeBlock});   
+      //          break
+      //        case 5:
+      //          day5array.push({user: user.userName, availability: timeBlock});  
+      //          break
+      //        case 6:
+      //          day6array.push({user: user.userName, availability: timeBlock});  
+      //          break
+      //        case 7:
+      //          day7array.push({user: user.userName, availability: timeBlock});    
+      //          break 
+          //  }
+      //     })
+      //     console.log(day1array, day2array, day0array, day3array, day4array, day5array, day6array, day7array)
+     
+
+   
+ 
+
+
+  // functtion that takes a day of the week array with availabilities and returns an array with the user's avilability at the alooted time
+      const addUserToDayArray = (dayArray:TimeArray[], event:UserInfo) => {
        
-     })
+        // get start and end for each event as Date object
+        let startObj = new DayPilot.Date(event.availability![0].start);
+        let endObj = new DayPilot.Date(event.availability![0].end);
+        let start = 
+           startObj.getHours() * 2 + 
+           (startObj.getMinutes() === 0 ?0 :1);
+         let end =
+           endObj.getHours() * 2 +
+           (endObj.getMinutes() === 0 ? 0 : 1);
+         // add user to day1results for the timeblock start and end
+         for(let i=start; i < end; i++){
+           dayArray[i].array.push({user: event.userName!, start: start, end: end})
+         }
+         return dayArray;
+      }
 
 
-    }
-    catch{
-      console.log("error")
-    }
-  }, [props.userInfoData])
+   useEffect(() => {
+
+     // for each day array, populate with the timeblocks for that day array
+       if (availabilityByDay!.sunday!.length > 0){
+         dayAvailabilityArray.sunday!.forEach(timeblock => {
+           let sundayResults = addUserToDayArray(day0array, timeblock)
+           console.log(sundayResults)
+         })
+       }
+       if (availabilityByDay!.monday!.length > 0){
+         dayAvailabilityArray.monday!.forEach(timeblock => {
+           let mondayResults = addUserToDayArray(day1array, timeblock)
+           console.log(mondayResults)
+         })
+       }
+       if (availabilityByDay!.tuesday!.length > 0){
+         dayAvailabilityArray.tuesday!.forEach(timeblock => {
+           let tuesdayResults = addUserToDayArray(day2array, timeblock)
+           console.log(tuesdayResults)
+         })
+       }
+      if (availabilityByDay!.wednesday!.length > 0){
+         dayAvailabilityArray.wednesday!.forEach(timeblock => {
+           let wednesdayResults = addUserToDayArray(day3array, timeblock)
+           console.log(wednesdayResults)
+         })
+       }
+      if (availabilityByDay!.thursday!.length > 0){
+         dayAvailabilityArray.thursday!.forEach(timeblock => {
+           let thursdayResults = addUserToDayArray(day4array, timeblock)
+           console.log(thursdayResults)
+         })
+       }
+       if (availabilityByDay!.friday!.length > 0){
+         dayAvailabilityArray.friday!.forEach(timeblock => {
+           let fridayResults = addUserToDayArray(day5array, timeblock)
+           console.log(fridayResults)
+         })
+       }
+      if (availabilityByDay!.saturday!.length > 0){
+         dayAvailabilityArray.saturday!.forEach(timeblock => {
+           let saturdayResults = addUserToDayArray(day6array, timeblock)
+           console.log(saturdayResults)
+         })
+       }
+       // eslint-disable-next-line
+   }, [availabilityByDay])
+
+
+
+    
   return(
     <>
+      <p>overlap calculations</p>
     </>
   )
 }
 
-export default overlap;
+export default Overlap;
