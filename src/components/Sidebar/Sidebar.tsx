c3ca822fd40d831493e2be78a169eed510e02cbd
John Marcos <johnsaguay@gmail.com>
2022-05-19 09:40:06 -0700

    testing out w/o sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -24,90 +24,90 @@ const Sidebar:React.FC<NameProps> = ({ userNames, numOfAttendees, results }) => 
   // create an array that holds user name or string with number for users that haven't entered availability yet
   // 0 if userNames.length === 1 ?userName[0] :"Coordinator"
   // 1 if userNames.length === 2 ?userName[1] :"Invitee 1"
-  // etc
-  useEffect(() => {
-    let namesToDisplay = numOfAttendees?.map((num) => {
-      let name = "";
-      let complete = false;
+  // // etc
+  // useEffect(() => {
+  //   let namesToDisplay = numOfAttendees?.map((num) => {
+  //     let name = "";
+  //     let complete = false;
 
-      switch(num){
-        case 1:
-          if (results && userNames && userNames.length >= 1){
-            name = userNames[0]!;
-            complete = true
-          } else if (results){
-            name = "Coordinator";
-            complete = false;
-          } else{
-            name = "Coordinator";
-            complete = true;
-          }
-          return {name: name, complete: complete}
-        case 2:
-          if (results && userNames && userNames.length >= 2){
-            name = userNames[1]!;
-            complete = true
-          } else if (results){
-            name = "Invitee 1";
-            complete = false;
-          }else {
-            name = "Invitee 1";
-            complete = true;
-          }
-          return {name: name, complete: complete}
-        case 3:
-          if (results && userNames && userNames.length >= 3){
-            name = userNames[2]!;
-            complete = true
-          } else if (results){
-            name = "Invitee 2";
-            complete = false;
-          }else {
-            name = "Invitee 2";
-            complete = true;
-          }         
-          return {name: name, complete: complete}
-        case 4:
-          if (results && userNames && userNames.length >= 4){
-            name = userNames[3]!;
-            complete = true
-          } else if(results){
-            name = "Invitee 3";
-            complete = false;
-          }else {
-            name = "Invitee 3";
-            complete = true;
-          }           
-          return {name: name, complete: complete}
-        case 5:
-          if (results && userNames && userNames.length >= 5){
-            name = userNames[4]!;
-            complete = true
-          } else if (results){
-            name = "Invitee 4";
-            complete = false;
-          }else{
-            name = "Invitee 4";
-            complete = true;
-          }           
-          return {name: name, complete: complete}
-        case 6:
-          if (results && userNames && userNames.length === 6){
-            name = userNames[5]!;
-            complete = true
-          } else if (results){
-            name = "Invitee 5";
-            complete = false;
-          }else {
-            name = "Invitee 5";
-            complete = true;
-          }           
-          return {name: name, complete: complete}
-      }
-    })
-    setAttendeeNames(namesToDisplay!);
+  //     switch(num){
+  //       case 1:
+  //         if (results && userNames && userNames.length >= 1){
+  //           name = userNames[0]!;
+  //           complete = true
+  //         } else if (results){
+  //           name = "Coordinator";
+  //           complete = false;
+  //         } else{
+  //           name = "Coordinator";
+  //           complete = true;
+  //         }
+  //         return {name: name, complete: complete}
+  //       case 2:
+  //         if (results && userNames && userNames.length >= 2){
+  //           name = userNames[1]!;
+  //           complete = true
+  //         } else if (results){
+  //           name = "Invitee 1";
+  //           complete = false;
+  //         }else {
+  //           name = "Invitee 1";
+  //           complete = true;
+  //         }
+  //         return {name: name, complete: complete}
+  //       case 3:
+  //         if (results && userNames && userNames.length >= 3){
+  //           name = userNames[2]!;
+  //           complete = true
+  //         } else if (results){
+  //           name = "Invitee 2";
+  //           complete = false;
+  //         }else {
+  //           name = "Invitee 2";
+  //           complete = true;
+  //         }         
+  //         return {name: name, complete: complete}
+  //       case 4:
+  //         if (results && userNames && userNames.length >= 4){
+  //           name = userNames[3]!;
+  //           complete = true
+  //         } else if(results){
+  //           name = "Invitee 3";
+  //           complete = false;
+  //         }else {
+  //           name = "Invitee 3";
+  //           complete = true;
+  //         }           
+  //         return {name: name, complete: complete}
+  //       case 5:
+  //         if (results && userNames && userNames.length >= 5){
+  //           name = userNames[4]!;
+  //           complete = true
+  //         } else if (results){
+  //           name = "Invitee 4";
+  //           complete = false;
+  //         }else{
+  //           name = "Invitee 4";
+  //           complete = true;
+  //         }           
+  //         return {name: name, complete: complete}
+  //       case 6:
+  //         if (results && userNames && userNames.length === 6){
+  //           name = userNames[5]!;
+  //           complete = true
+  //         } else if (results){
+  //           name = "Invitee 5";
+  //           complete = false;
+  //         }else {
+  //           name = "Invitee 5";
+  //           complete = true;
+  //         }           
+  //         return {name: name, complete: complete}
+  //     }
+  //   })
+  //   setAttendeeNames(namesToDisplay!);
   
-  }, [userNames, numOfAttendees, results])
+  // }, [userNames, numOfAttendees, results])
  
   
 
